f1482e79b80aca13b3b2d457e98d35638c166970
Jean Meira <jean.meira10@hotmail.com>
2022-01-11 14:39:28 -0300

    feat: validation user

--- src/models/User.js
+++ src/models/User.js
@@ -4,10 +4,39 @@ export default class User extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        sobrenome: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password_hash: Sequelize.STRING,
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [4, 20],
+              msg: 'Campo nome deve ter entre 4 e 20 caracteres',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            isEmail: {
+              msg: 'E-mail inválido',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: 'A senha deve ter entre 6 e 50 caracteres',
+            },
+          },
+        },
       },
       {
         sequelize,
