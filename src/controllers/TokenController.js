11509d461e109632ebfa078232a9f9f1833c7c7e
Jean Meira <jean.meira10@hotmail.com>
2022-01-12 17:48:10 -0300

    feat: update token generate

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -1,8 +1,37 @@
 import User from '../models/User';
+import jwt from 'jsonwebtoken';
 
 class TokenController {
   async store(req, res) {
-    res.json('ok');
+    const { email = '', password = '' } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        errors: ['Credências inválidas'],
+      });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({
+        errors: ['Usuário não existe'],
+      });
+    }
+
+    if (!(await user.passwordIsValid(password))) {
+      return res.status(401).json({
+        errors: ['Senha inválida'],
+      });
+    }
+
+    const { id } = user;
+
+    const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    });
+
+    return res.json({ token });
   }
 }
 
