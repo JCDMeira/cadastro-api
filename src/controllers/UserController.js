e759d4728fb9135b61662e87907483bfc09ab3a6
Jean Meira <jean.meira10@hotmail.com>
2022-01-12 15:50:02 -0300

    feat: update users, dinamic info

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,11 +3,7 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
-      const novoUser = await User.create({
-        nome: 'Jean',
-        email: 'jean.meira10@hotmail.com',
-        password: '123456',
-      });
+      const novoUser = await User.create(req.body);
       res.json(novoUser);
     } catch (e) {
       res.status(400).json({
