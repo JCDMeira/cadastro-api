5aa5e7a63228ba959de3441d99eabb50c8a17a45
Jean Meira <jean.meira10@hotmail.com>
2022-01-12 15:58:51 -0300

    feat: add get all users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,32 @@
 import User from '../models/User';
 
 class UserController {
+  // # store
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
   }
+
+  // # index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+  // # show
+
+  // # update
+
+  // # delete
 }
 
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
 
 export default router;
 
