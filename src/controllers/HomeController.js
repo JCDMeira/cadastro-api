307e06b067fa66c2042c67429f10077460ea535d
Jean Meira <jean.meira10@hotmail.com>
2022-01-14 16:53:23 -0300

    refactor: clean HomeController

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,15 +1,6 @@
-import Aluno from '../models/Aluno';
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Jean',
-      sobrenome: 'Meira',
-      email: 'jean.meira10@hotmail.com',
-      idade: 24,
-      peso: 60,
-      altura: 1.78,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
