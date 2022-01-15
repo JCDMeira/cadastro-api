db86c2149c11850d1c1f286380c5656164220281
Jean Meira <jean.meira10@hotmail.com>
2022-01-15 20:14:32 -0300

    feat: update photo upload

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,22 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json('Foto upada com sucesso!');
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
+    {
+      return;
+    }
   }
 }
 
